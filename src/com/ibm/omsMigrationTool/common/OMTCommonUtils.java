7f0e090dbfdb65f75846ecb0bf08c882adaaf242
ANNAMALAI RAJENDRAN <annamalai.rajendran@us.ibm.com>
2019-07-18 15:35:49 -0500

    added a method to convert DocumentTOSTring

--- src/com/ibm/omsMigrationTool/common/OMTCommonUtils.java
+++ src/com/ibm/omsMigrationTool/common/OMTCommonUtils.java
@@ -1,7 +1,17 @@
 package com.ibm.omsMigrationTool.common;
 
+import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
 
 public final class OMTCommonUtils implements OMTConstants{
 	//Private constructor
@@ -17,5 +27,14 @@ public final class OMTCommonUtils implements OMTConstants{
 		
 		return propertyValue; 
 		
+	}
+	
+	public static String documentToString(Document inDoc) throws TransformerException
+	{
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer trans = tf.newTransformer();
+		StringWriter sw = new StringWriter();
+		trans.transform(new DOMSource(inDoc), new StreamResult(sw));
+		return sw.toString();
 	}
 }
